3af2f916ab2a0b984b835441010e94d48abe405e
MiguelBarreraD <diazbarrera90355@gmail.com>
2024-04-05 09:34:17 -0500

    Update URL

--- src/main/java/org/example/proxyServer.java
+++ src/main/java/org/example/proxyServer.java
@@ -5,15 +5,15 @@ import static spark.Spark.*;
 import java.io.IOException;
 
 public class proxyServer {
-    private static final String MATH_SERVICE_1_URL = "ec2-18-209-43-76.compute-1.amazonaws.com:4567";
-    private static final String MATH_SERVICE_2_URL = "ec2-54-80-171-222.compute-1.amazonaws.com:4567";
+    private static final String MATH_SERVICE_1_URL = "http://ec2-user@ec2-18-209-43-76.compute-1.amazonaws.com:4567";
+    private static final String MATH_SERVICE_2_URL = "http://ec2-user@ec2-54-80-171-222.compute-1.amazonaws.com:4567";
     
 
     private static int currentServiceIndex = 0;
 
     public static void main(String[] args) {
         port(getPort());
-        staticFiles.location("/public");
+        staticFiles.location("/public");    
 
         get("linealsearch", (req, res) -> {
             String listNumbersParam = req.queryParams("list");
